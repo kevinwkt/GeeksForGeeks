39a5670e336633f95c5d74de6a29d8fc3db228a9
kaero <kevinwkt97@outlook.com>
2017-02-08 19:16:54 -0600

    CLion

---
+++ CLion/jumpsearch/cmake-build-debug/CMakeFiles/jumpsearch.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/jumpsearch.dir/main.cpp.obj"
+  "jumpsearch.pdb"
+  "jumpsearch.exe"
+  "jumpsearch.exe.manifest"
+  "libjumpsearch.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/jumpsearch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
