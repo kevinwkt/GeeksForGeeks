21c561f0727083a31a76bf8e8fc8c2b2c90dff0d
kaero <kevinwkt97@outlook.com>
2017-02-08 19:30:07 -0600

    CLion

---
+++ CLion/interpolationsearch/cmake-build-debug/CMakeFiles/interpolationsearch.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/interpolationsearch.dir/main.cpp.obj"
+  "interpolationsearch.pdb"
+  "interpolationsearch.exe"
+  "interpolationsearch.exe.manifest"
+  "libinterpolationsearch.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/interpolationsearch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
