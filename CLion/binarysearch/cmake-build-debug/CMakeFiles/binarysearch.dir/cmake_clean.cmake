7d5d079bbbb50d500a282e4d7f5fb957d93bb462
kaero <kevinwkt97@outlook.com>
2017-02-08 18:55:12 -0600

    CLion

---
+++ CLion/binarysearch/cmake-build-debug/CMakeFiles/binarysearch.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/binarysearch.dir/main.cpp.obj"
+  "binarysearch.pdb"
+  "binarysearch.exe"
+  "binarysearch.exe.manifest"
+  "libbinarysearch.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/binarysearch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
